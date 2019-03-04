067e5ac64e4c904cb4ee7656c4ddfd010f090157
Prateek Panwar <prateekpanwar31@gmail.com>
2019-03-04 17:08:52 -0500

    Bitcoin Wallet Classifier API

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ Created on Fri Dec 21 13:38:04 2018
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from classification_v2 import label_address
-import json
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -21,8 +21,15 @@ class User(Resource):
         final_labels = label_address(address)
         if len(final_labels) != 0:
             return (final_labels.to_json(), 200)
-        return ("Not found", 404)
+        return ("Not founds", 404)
 
+class instruct(Resource):
+    def get(self):
+        return('Usage: https://bitcoin-wallet-classifier.herokuapp.com/wallet-addresses OR https://bitcoin-wallet-classifier.herokuapp.com/address1,address2...')
+        
 api.add_resource(User, "/<string:address>")
-app.run(debug=True)
+api.add_resource(instruct, "/")
+
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=False, port=port, host='0.0.0.0')
 
